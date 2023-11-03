ac8355745da68abf17d48ed2d8013cb85f64a2ed
Samuel Klein <klein@bio.ifi.lmu.de>
2023-11-03 17:34:02 +0100

    added functionality to check jar with external javafx libraries, added custom container to workflow

--- src/test/test-jar-executable.sh
+++ src/test/test-jar-executable.sh
@@ -11,7 +11,7 @@ if [[ $num_jars -ne 1 ]]; then
     exit 1
 fi
 
-# a manifest is a neccessary condition for it to be an executable jar file
+# a manifest is a necessary condition for it to be an executable jar file
 if jar -tf "$jar_file" | grep -q "^META-INF/MANIFEST.MF$"; then
     echo "[ INFO ] jar file contains a manifest."
 else
@@ -19,14 +19,46 @@ else
     exit 1
 fi
 
-# timeout the application because sometimes the process waits for user input (ignore program stdout)
-timeout --foreground 5s java -jar "$jar_file" > /dev/null
-
+# timeout the application because sometimes the process waits for user input
+message=$(timeout --foreground 5s java -jar "$jar_file" 2>&1)
 # the exit code of the jar execution
 exit_code=$?
 
 # the exit code 124 indicates that the process was terminated by timeout -> jar is executable
 if [[ $exit_code -eq 124 ]]; then
+    echo "[ INFO ] process was terminated by timeout."
+    exit_code=0
+fi
+
+if [[ $exit_code -eq 0 ]]; then
+    echo "[ OK ] jar file is executable."
+    exit 0
+else
+    echo "[ WARN ] exit code is $exit_code"
+    echo "[ INFO ] error message is:"
+    echo "$message"
+fi
+
+if echo "$message" | grep -qiv "javafx"; then
+    echo "[ INFO ] error message does not mention javafx."
+    echo "[ ERROR ] jar file is not executable."
+    exit 1
+fi
+
+echo "[ INFO ] jar file seems to require javafx libraries."
+echo "[ INFO ] starting a new virtual display.."
+
+# start the virtual display
+Xvfb :99 -screen 0 1024x768x24 &
+
+# try to run the jar with the javafx libraries
+timeout --foreground 5s java -Djava.library.path=/usr/local/openjdk-11/ --module-path "/javafx-sdk-17.0.9/lib/" --add-modules "javafx.base,javafx.controls,javafx.graphics,javafx.fxml,javafx.media,javafx.swing" -jar "$jar_file"
+# the exit code of the jar execution
+exit_code=$?
+
+# the exit code 124 indicates that the process was terminated by timeout -> jar is executable
+if [[ $exit_code -eq 124 ]]; then
+    echo "[ INFO ] process was terminated by timeout."
     exit_code=0
 fi
 
