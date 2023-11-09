8e5964ec6d8adf7a4759f696abd4da2f9e2e13b5
Malte Weyrich <malte.weyrich@hotmail.de>
2023-11-09 22:08:10 +0100

    Player class (still not sure if i need that)

--- src/main/java/de/lmu/bio/ifi/PlayerClass.java
+++ src/main/java/de/lmu/bio/ifi/PlayerClass.java
@@ -1,2 +1,46 @@
-package de.lmu.bio.ifi;public class PlayerClass {
+package de.lmu.bio.ifi;
+
+import szte.mi.Move;
+import szte.mi.Player;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PlayerClass implements Player {
+        private long timeLeft;
+        private Random rnd;
+        private OthelloGame othelloGame;
+        private int order;
+        private int points;
+        private PlayerType playerType;
+
+        public long getTimeLeft() {
+            return timeLeft;
+        }
+
+        public void init(int order, long t, java.util.Random rnd){
+            this.othelloGame = new OthelloGame(order);
+            this.timeLeft = t;
+            this.rnd = rnd;
+            this.order = order;
+            this.playerType = PlayerType.PLAYER;
+        }
+
+    public PlayerType getPlayerType() {
+        return playerType;
+    }
+
+    public void incrementPoints(int points){
+            this.points+=points;
+        }
+    public Move nextMove(Move prevMove, long tOpponent, long t){
+        if (prevMove != null){ // append move to game
+            othelloGame.makeMove(order==1, prevMove.x, prevMove.y);
+        }
+
+        // react to a previous move
+        ArrayList<Move> possibleMoves = (ArrayList<Move>) othelloGame.getPossibleMoves(order==0);
+
+        return possibleMoves.get(0);
+    }
 }
