063c6c712354dea09ca7c529998978aee89d1d46
Malte Weyrich <malte.weyrich@hotmail.de>
2023-11-12 16:27:04 +0100

    testing edgecase

--- src/main/java/de/lmu/bio/ifi/Runner.java
+++ src/main/java/de/lmu/bio/ifi/Runner.java
@@ -34,9 +34,32 @@ public class Runner {
 
         FileReader reader = new FileReader(new File("/home/malte/01_Documents/projects/othello/src/main/java/de/lmu/bio/ifi/moves.tsv"));
         ArrayList<Move> moves = readMoves(reader);
-
-
+        OthelloGame o = new OthelloGame(0);
         int c = 0;
+        // for (Move m : moves) {
+        //     o.makeMove(c%2==0, m.x, m.y);
+        //     System.out.println(o.gameStatus().toString());
+        //     System.out.println(o.toString());
+        //     System.out.println("Next move: " + o.getCurrPlayer());
+        //     if (o.getPossibleMoves(false) == null && o.getPossibleMoves(true) == null) {
+        //         System.out.println("Moves for player 1: null");
+        //         System.out.println("Moves for player 2: null");
+        //     }
+        //     else if (o.getPossibleMoves(true) == null){
+        //         System.out.println("Moves for player 1: null");
+        //         System.out.println("Moves for player 2: " +o.getPossibleMoves(false).size());
+        //     }
+        //     else if (o.getPossibleMoves(false) == null){
+        //         System.out.println("Moves for player 1: " +o.getPossibleMoves(true).size());
+        //         System.out.println("Moves for player 2: null");
+        //     }
+        //     else {
+        //         System.out.println("Moves for player 1: " +o.getPossibleMoves(true).size());
+        //         System.out.println("Moves for player 2: " +o.getPossibleMoves(false).size());
+        //     }
+        //     c++;
+        //     System.out.println(o.gameStatus().toString());
+        // }
         OthelloGui.main(args);
     }
 }
